6162bd7062ae728911b10f1aa20773c0b606fdf7
Ali <alihajivand14@gmail.com>
2023-02-10 00:41:44 +0330

    ..

--- DisciplinaryCase/Models/Discipline.cs
+++ DisciplinaryCase/Models/Discipline.cs
@@ -2,10 +2,26 @@
 {
     public class Discipline
     {
+      
         public int id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ClassId { get; set; }
         public int NationalCode { get; set; }
+        public string BodyDiscipline { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public Discipline(int id, string firstName, string lastName, string classId, int nationalCode, string bodyDiscipline, DateTime creationDate)
+        {
+            this.id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            ClassId = classId;
+            NationalCode = nationalCode;
+            BodyDiscipline = bodyDiscipline;
+            CreationDate = DateTime.Now;
+        }
     }
+
+
 }
