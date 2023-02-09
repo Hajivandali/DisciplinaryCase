66b437fb2f2622da44d97c64f3621c987a539bd4
Ali <alihajivand14@gmail.com>
2023-02-10 00:24:31 +0330

    createDiscipline

---
+++ DisciplinaryCase/Models/Discipline.cs
@@ -0,0 +1,11 @@
+﻿namespace DisciplinaryCase.Models
+{
+    public class Discipline
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassId { get; set; }
+        public int NationalCode { get; set; }
+    }
+}
---
+++ DisciplinaryCase/Pages/CreateDiscipline.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DisciplinaryCase.Pages
+{
+    public class CreateDisciplineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
